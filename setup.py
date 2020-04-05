84bb5756f9df8392446588e7609c5abc48592f9b
Joe Coburn <coburnj11@gmail.com>
2020-04-05 20:35:52 +0100

    Corrected name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bussing",
+    name="bus",
     version="0.0.1",
     author="Joe Coburn",
     author_email="joe@scholarpack.com",
